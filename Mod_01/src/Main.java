759b2de58e0236bd519e422cd12ca2152a708ef2
Randolfo <ragnifico@yahoo.com.br>
2024-03-02 23:41:52 -0300

    mkdir src

---
+++ Mod_01/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ *  Main 
+ * ======
+ * @author: Randolfo
+ * @since:  02/02/24
+ * @file:   Main.java 
+ *
+ * Classe dedicada para chamada e gestão do programa.
+*/
+
+public class Main{
+
+    public static void main(String[] argv){
+        System.out.println("Ola mundo");  
+    }
+}
