607210f7b93212535c991f34071e10824ecb1d1f
Randolfo <ragnifico@yahoo.com.br>
2024-03-15 13:34:35 -0300

    Botoes animados

--- sql/inser_data.sql
+++ sql/inser_data.sql
@@ -9,30 +9,30 @@ INSERT INTO Categories (_name,_brief,_color) VALUES ('Text','Representa stream d
 
 INSERT INTO Categories (_name,_brief,_color) VALUES ('Logic','Representa os valores lógicos true e false','FF00FF');
 
-INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('byte','Integer','type_byte.png','byte x=(byte) 1;',8,'MIN=-128 MAX=127');
+INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('byte','Integer','type_byte.png','byte x=(byte) 1;',8,'<br> MIN=-128 <br> MAX=127');
 
 
-INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('short','Integer','type_short.png','short x=(short) 1;',16,'MIN=-32768 MAX=32767');
+INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('short','Integer','type_short.png','short x=(short) 1;',16,'<br> MIN=-32768 <br> MAX=32767');
 
 
-INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('int','Integer','type_int.png','int x= 1;',32,'MIN=-2147483648 MAX=2147483647');
+INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('int','Integer','type_int.png','int x= 1;',32,'<br> MIN=-2147483648 <br> MAX=2147483647');
 
 
-INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('long','Integer','type_long.png','long x= 1l;',64,'MIN=-9223372036854770000 MAX=9223372036854770000');
+INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('long','Integer','type_long.png','long x= 1l;',64,'<br> MIN=-9223372036854770000 <br> MAX=9223372036854770000');
 
 
-INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('float','Real','type_float.png','float x= 1.5f;',32,'MIN=-1.4024E-37 MAX=3.40282347E+38');
+INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('float','Real','type_float.png','float x= 1.5f;',32,'<br> MIN=-1.4024E-37 <br> MAX=3.40282347E+38');
 
 
-INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('double','Real','type_double.png','double x= 1.450;',64,'MIN=-4.94E-307 MAX=1.79769313486231570E + 308');
+INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('double','Real','type_double.png','double x= 1.450;',64,'<br> MIN=-4.94E-307 <br> MAX=1.79769313486231570E + 308');
 
 
-INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('char','Text','type_char.png','char x=''A'';',16,'MIN=0 MAX=65535');
+INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('char','Text','type_char.png','char x=''A'';',16,'<br> MIN=0 <br> MAX=65535');
 
 
-INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('string','Text','type_string.png','String x="abc";',64,'MIN=0 MAX=MAX=179769313486231570E + 308');
+INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('string','Text','type_string.png','String x="abc";',64,'<br> MIN=0 <br> MAX=179769313486231570E + 308');
 
 
-INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('bool','Logic','type_bool.png','Boolean x=true;',1,'MIN=0 MAX=1');
+INSERT INTO Kinds (_name,_name_ctg,_icon,_exemple,_size,_extension) VALUES ('bool','Logic','type_bool.png','Boolean x=true;',1,'<br>MIN=0 <br> MAX=1');
 
 
--- src/Gui.java
+++ src/Gui.java
@@ -13,11 +13,18 @@ import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+
+import java.awt.BorderLayout;
 import java.awt.event.*;
+import java.awt.CardLayout;
+import java.awt.Container;
+import java.awt.Font;
 
 public class Gui {
 //  Fields
@@ -33,7 +40,14 @@ public class Gui {
     
     // Panal
     private JPanel main_panel = new JPanel(),                  //< Painel principal
-                   data_panel = new JPanel();                  //< Painel de dados 
+                   data_panel = new JPanel(new BorderLayout());                  //< Painel de dados 
+    
+    private CardLayout panel  = new CardLayout();
+    private Container  buff   = null;
+    private JLabel info = new JLabel(),
+                   icon = new JLabel("    "),
+                   space= new JLabel("     ");
+
 // Menus:
     private String[] edit={"Edit","Create","Delete"},          //< Edição
                      find={"Categoria","Nome"};                //< Busca
@@ -48,6 +62,7 @@ public class Gui {
        connect_bank(path);
        draw_windown(path);
        plug_components();
+
     }
 
 //  Exibir Janela
@@ -56,9 +71,15 @@ public class Gui {
 
 //---------------------------------> Plug <----------------------------------
     void plug_components(){
-       wd.add(data_panel);
-       wd.add(main_panel);
        wd.setJMenuBar(mbar);
+       buff= wd.getContentPane();    // Contem componentes
+       buff.setLayout(panel);        // Faz a gestão
+       buff.add(main_panel);         // new Component
+       JPanel p=new JPanel();
+       p.add(icon);
+       p.add(info);
+       data_panel.add(p,BorderLayout.CENTER);
+       buff.add(data_panel);         // new componentes
     }
 //---------------------------------> Bank <----------------------------------
     void connect_bank(String path){
@@ -80,13 +101,22 @@ public class Gui {
         for(var type:types){
             JButton button =new JButton(new ImageIcon(path+type.get_icon()));  
             button.addActionListener(new ActionListener(){
-               public void actionPerformed(ActionEvent e) { data(type);}
+               public void actionPerformed(ActionEvent e) { data(type, path+type.get_icon());}
             }); 
             button.setOpaque(false);
             button.setContentAreaFilled(false);
             button.setBorderPainted(false);            
             main_panel.add(button);   
         }
+            JButton button =new JButton(new ImageIcon(path+"re_96.png"));  
+            button.setOpaque(false);
+            button.setContentAreaFilled(false);
+            button.setBorderPainted(false);
+            
+            button.addActionListener(new ActionListener(){
+               public void actionPerformed(ActionEvent e) { exit();}
+            });
+            data_panel.add(button,BorderLayout.NORTH);
     }
 
 // Draw windown:
@@ -118,12 +148,23 @@ public class Gui {
     }
 
 //---------------------------------> Events <----------------------------------
-    void data(Types type){
+    void data(Types type,String path){
         bank.select(type);
-        main_panel.setVisible(false); 
-        data_panel.setVisible(true);
-        
+        info.setText(
+        "<html>"
+        +"<br><h1><u>Name: "+type.get_name()+"</u></h1>"
+        +"<br>Category: "+type.get_category_name()
+        +"<br>Ex: "+type.get_example()
+        +"<br>Size: "+type.get_size()
+        +"<br>Extension: "+type.get_extension()
+        +"</html>");
+        icon.setIcon(new ImageIcon(path));
+        info.setFont(new Font("Serif", Font.BOLD, 18));
+        panel.next(buff);
     }
-      
+     
+    void exit(){
+        panel.next(buff);
+    }
 }
 
